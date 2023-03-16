54e902faecdfc2b4532caaea926d7fa66136202b
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-16 16:10:49 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
+    private
+
+    def render_not_found_response
+        @model_name = controller_name.classify
+        render json: {errors: "#{@model_name} not found"}, status: :not_found
+    end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,29 @@
 class ItemsController < ApplicationController
-
+  wrap_parameters format: []
   def index
-    items = Item.all
+    if params[:user_id]
+      user= User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
